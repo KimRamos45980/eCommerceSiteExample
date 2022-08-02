5d29d28c1883b1b0d72073bd706d1288c556accd
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-02 14:34:23 -0700

    Added more columns migration to games table

---
+++ eCommerceSiteExample/Migrations/20220802213144_AddedGameColumns.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerceSiteExample.Migrations
+{
+    public partial class AddedGameColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Device",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Device",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Games");
+        }
+    }
+}

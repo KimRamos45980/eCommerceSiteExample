5975dbbf4ae15fc23e3db8b883d15fa5ede6a711
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-12 13:54:26 -0700

    Create game fully functional

--- eCommerceSiteExample/Controllers/GamesController.cs
+++ eCommerceSiteExample/Controllers/GamesController.cs
@@ -1,14 +1,37 @@
-﻿using eCommerceSiteExample.Models;
+﻿using eCommerceSiteExample.Data;
+using eCommerceSiteExample.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSiteExample.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly VideoGameContext _context;
+
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Games.Add(game); // Prepares insert
+                _context.SaveChanges();   // Execture pending insert
+
+                ViewData["Message"] = $"{game.Title} was added successfully!";
+                return View();
+            }
+
+            return View(game);
         }
     }
 }

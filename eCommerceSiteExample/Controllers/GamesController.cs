bff71f21eb76e8ec4c05ee40d7273f3eb35a89a3
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-12 13:34:14 -0700

    Made a Create Game view

---
+++ eCommerceSiteExample/Controllers/GamesController.cs
@@ -0,0 +1,14 @@
+﻿using eCommerceSiteExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSiteExample.Controllers
+{
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

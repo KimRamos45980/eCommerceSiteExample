c2d2e624ba2dd94d18559eea228bcdabe69aeab6
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-21 12:46:26 -0700

    Added register view

---
+++ eCommerceSiteExample/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSiteExample.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}

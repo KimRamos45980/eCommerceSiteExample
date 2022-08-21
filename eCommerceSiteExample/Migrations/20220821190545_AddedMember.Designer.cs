0280863546e44cc5024f9cd25e60180c472cdc4c
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-21 12:07:43 -0700

    Added migration for member class

--- eCommerceSiteExample/Migrations/20220821190545_AddedMember.Designer.cs
+++ eCommerceSiteExample/Migrations/20220821190545_AddedMember.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerceSiteExample.Data;
 
@@ -10,9 +11,10 @@ using eCommerceSiteExample.Data;
 namespace eCommerceSiteExample.Migrations
 {
     [DbContext(typeof(VideoGameContext))]
-    partial class VideoGameContextModelSnapshot : ModelSnapshot
+    [Migration("20220821190545_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

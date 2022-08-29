0a4f28a6d735537cae6da5f1c3df4deab276c073
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-29 15:19:35 -0700

    Created Add to Cart button

---
+++ eCommerceSiteExample/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using eCommerceSiteExample.Data;
+using eCommerceSiteExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSiteExample.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly VideoGameContext _context;
+
+        public CartController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Game gameToAdd = _context.Games.Where(g => g.GameId == id).SingleOrDefault();
+
+            if (gameToAdd == null)
+            {
+                // Game with specified id does not exist
+                TempData["Message"] = "Sorry that game no longer exists";
+                return RedirectToAction("Index", "Games");
+            }
+
+            // Todo: Add item to a shopping cart cookie
+            TempData["Message"] = "Item added to cart";
+            return RedirectToAction("Index", "Games");
+        }
+    }
+}

a8567758d74d2813b3975ba491e5b1d022061fb8
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-29 15:32:05 -0700

    Shopping cart cookies stores mulitple games

--- eCommerceSiteExample/Controllers/CartController.cs
+++ eCommerceSiteExample/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using eCommerceSiteExample.Data;
 using eCommerceSiteExample.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerceSiteExample.Controllers
 {
     public class CartController : Controller
     {
         private readonly VideoGameContext _context;
+        private const string Cart = "ShoppingCart";
 
         public CartController(VideoGameContext context)
         {
@@ -24,9 +26,45 @@ namespace eCommerceSiteExample.Controllers
                 return RedirectToAction("Index", "Games");
             }
 
+            CartGameViewModel cartGame = new()
+            {
+                GameId = gameToAdd.GameId,
+                Title = gameToAdd.Title,
+                Device = gameToAdd.Device,
+                Price = gameToAdd.Price,
+                Region = gameToAdd.Region
+            };
+
+            List<CartGameViewModel> cartGames = getExistingCartData();
+            cartGames.Add(cartGame);
+
+            string cookieData = JsonConvert.SerializeObject(cartGames);
+
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
+
             // Todo: Add item to a shopping cart cookie
             TempData["Message"] = "Item added to cart";
             return RedirectToAction("Index", "Games");
+        }
+
+        /// <summary>
+        /// Return the current list of video games in the users shopping
+        /// cart cookie. If there is no cookie, an empty list will be returned
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private List<CartGameViewModel> getExistingCartData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartGameViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<CartGameViewModel>>(cookie);
         }
     }
 }
